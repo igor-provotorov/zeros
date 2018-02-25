e835e04fc1085f89fef8809763e32bccd4e646d8
igor-provotorov <garynya@yandex.ru>
2018-02-25 22:29:30 +0300

    zeros/feature: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+  let result = 0;
+
+  for (let i = 5; i <= number; i *= 5) {
+    result += parseInt(number / i);
+  }
+
+  return result;
 }
